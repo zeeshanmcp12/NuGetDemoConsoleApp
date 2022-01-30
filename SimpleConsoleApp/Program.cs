b5fc3a09a281150908078bd710176c6126beb98a
Muhammad Zeeshan <mze@matas.dk>
2022-01-31 00:07:24 +0500

    Add project files.

---
+++ SimpleConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ACloudTechie.Artifact;
+
+namespace SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
