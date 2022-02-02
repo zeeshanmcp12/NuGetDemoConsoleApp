9e1400620698746ce5cd96fff1664e940585df47
Muhammad Zeeshan <mze@matas.dk>
2022-02-02 19:56:43 +0500

    add multiply method for multiplication

--- SimpleConsoleApp/Program.cs
+++ SimpleConsoleApp/Program.cs
@@ -9,6 +9,7 @@ namespace SimpleConsoleApp
         {
             //Console.WriteLine("Hello World!");
             Console.WriteLine($"Sum of 5 and 6 is { Calculator.Add(5, 6) } ");
+            Console.WriteLine($"5 * 6 is { Calculator.Multiply(5, 6) } ");
         }
     }
 }
